3d102c9c4a26d1dd21a248e0057bbd938e3398bf
Antonio Gantolea <antoniogantolea@yahoo.it>
2024-05-21 17:33:51 +0200

    bootstrap martedì

--- 21-maggio-24/Hola/src/app/app.module.ts
+++ 21-maggio-24/Hola/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
@@ -18,7 +19,8 @@ import { Componente3Component } from './components/componente3/componente3.compo
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
